c396bc5b8df75cc705085b236c5a864cc04eff37
NehuenYago <NehuenRomero@protonmail.com>
2022-03-29 10:10:37 -0300

    create App, Header, Container components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>header</h1>
+    )
+  }
+}
+
+class Container extends React.Component {
+  render() {
+    return (
+      <h1>container</h1>
+    )
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Container />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-    <h1> Hello World!</h1>,
+    <App />,
     document.getElementById('root'), //root DOM node
   );
